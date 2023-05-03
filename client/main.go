629d6a4e13c30b80f15934549be687646dc4c8e6
rshezarr <rshezarr@gmail.com>
2023-05-04 00:36:16 +0600

    call: call function

--- client/main.go
+++ client/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	pb "grpc-demo/proto"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8081"
+)
+
 func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Error while connection: %e", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewGreetServiceClient(conn)
+
+	names := &pb.NamesList{
+		Names: []string{"John", "Adam", "Noah"},
+	}
+
+	callSayHelloClientStream(client, names)
 
 }
